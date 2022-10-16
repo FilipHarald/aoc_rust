f620061aa2f3c639734d2286f9a6b2718d767759
filip <filip.harald@gmail.com>
2022-10-16 21:26:44 +0200

    Day 2

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO
+file reader
+better folder structure
+filter tests
+implement something for run function?
--- src/puzzles.rs
+++ src/puzzles.rs
@@ -1,5 +1,7 @@
 pub mod p_2015_day_1;
+pub mod p_2015_day_2;
 
 pub fn run_all() {
     p_2015_day_1::solver::run();
+    p_2015_day_2::solver::run();
 }
