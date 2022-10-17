811b26eb5132cdf0f72dbdbb1c95d38723f37bcc
filip <filip.harald@gmail.com>
2022-10-17 23:27:34 +0200

    Some comments.

--- src/puzzles/p_2015_day_3/solver.rs
+++ src/puzzles/p_2015_day_3/solver.rs
@@ -27,6 +27,7 @@ fn solve_a(input: &str) -> usize {
 
 fn move_and_deliver(x: &mut i32, y: &mut i32, c: char, vistited_houses: &mut HashMap<Vec<i32>, i32>) {
         match c {
+            // D: * I'm not sure yet why dereferencing helps here.
             '^' => *x += 1,
             'v' => *x -= 1,
             '>' => *y += 1,
@@ -38,10 +39,13 @@ fn move_and_deliver(x: &mut i32, y: &mut i32, c: char, vistited_houses: &mut Has
 
 fn solve_b(input: &str) -> usize {
     let mut vistited_houses = HashMap::<Vec<i32>, i32>::new();
-    let mut santa_x = 0;
+    let mut santa_x = 0; // A: mutable here because we are going to change this var.
     let mut santa_y = 0;
     let mut robot_x  = 0;
     let mut robot_y = 0;
+    // B: & here because we want to make sure that it is the reference we are passing on and not
+    // the value.
+    // C: mut here because we want the reference we pass on to be mutable.
     move_and_deliver(&mut santa_x, &mut santa_y, 'x', &mut vistited_houses);
     move_and_deliver(&mut robot_x, &mut robot_y, 'x', &mut vistited_houses);
     for (index, c) in input.chars().enumerate() {
