edfb521d64acd44803b349586bebeb94e76e498d
filip <filip.harald@gmail.com>
2022-11-25 08:56:55 +0100

    2021 day 12 part a & b

--- src/puzzles.rs
+++ src/puzzles.rs
@@ -6,6 +6,8 @@ pub mod p_2015_day_5;
 pub mod p_2015_day_6;
 pub mod p_2015_day_7;
 
+pub mod p_2021_day_12;
+
 // TODO: some kind of hash map?
 //const PS: HashMap<&str, fn(&str) -> i32> = HashMap::from([
 //                         ("20151a", p_2015_day_1::solver::solve_a as fn(&str) -> i32)
@@ -34,6 +36,8 @@ pub fn solve(year: &str, day: &str, part: &str, input: &str) -> i32 {
         "20156b"=>p_2015_day_6::solver::solve_b(input),
         "20157a"=>p_2015_day_7::solver::solve_a(input),
         "20157b"=>p_2015_day_7::solver::solve_b(input),
+        "202112a"=>p_2021_day_12::solver::solve_a(input),
+        "202112b"=>p_2021_day_12::solver::solve_b(input),
         _=>todo!(),
     };
     return result;
---
+++ src/puzzles/p_2021_day_12/solver.rs
@@ -0,0 +1,188 @@
+use std::collections::HashMap;
+
+fn parse(lines: &str) -> HashMap<&str, Vec<&str>> {
+    let mut caves: HashMap<&str, Vec<&str>> = HashMap::new();
+    for l in lines.lines() {
+        let cave_edge: Vec<&str> = l.split("-").collect();
+        caves
+            .entry(cave_edge[0])
+            .or_insert(Vec::new())
+            .push(cave_edge[1]);
+        caves
+            .entry(cave_edge[1])
+            .or_insert(Vec::new())
+            .push(cave_edge[0]);
+    }
+    return caves;
+}
+
+fn check_is_valid_cave_connection(
+    cave_id: &str,
+    path_to_this_cave: &Vec<&str>,
+    has_visited_small_cave_twice: bool,
+) -> (bool, bool) {
+    if cave_id.to_uppercase() == cave_id {
+        return (true, has_visited_small_cave_twice);
+    }
+    if cave_id == "start" {
+        return (false, has_visited_small_cave_twice);
+    }
+    let cave_id_has_been_visited = path_to_this_cave.contains(&cave_id);
+    if has_visited_small_cave_twice {
+        return (!cave_id_has_been_visited, has_visited_small_cave_twice);
+    }
+    return (true, cave_id_has_been_visited);
+}
+
+fn recursive_find_possible_end_paths<'a>(
+    connections: &HashMap<&str, Vec<&'a str>>,
+    path_to_this_cave: Vec<&'a str>,
+    has_already_visited_small_cave_twice: bool,
+) -> Vec<Vec<&'a str>> {
+    let current_cave_id = path_to_this_cave.last().unwrap();
+    connections.get(current_cave_id).unwrap().iter().fold(
+        Vec::new(),
+        |mut possible_end_paths, connection_cave_id| {
+            let (is_valid_cave_connection, has_visited_small_cave_twice) =
+                check_is_valid_cave_connection(
+                    connection_cave_id,
+                    &path_to_this_cave,
+                    has_already_visited_small_cave_twice,
+                );
+            if is_valid_cave_connection {
+                let mut path = path_to_this_cave.clone();
+                path.push(connection_cave_id);
+                if connection_cave_id.to_owned() == "end" {
+                    possible_end_paths.push(path)
+                } else {
+                    let mut more_possible_end_paths = recursive_find_possible_end_paths(
+                        &connections,
+                        path,
+                        has_visited_small_cave_twice,
+                    );
+                    possible_end_paths.append(&mut more_possible_end_paths);
+                }
+            }
+            return possible_end_paths;
+        },
+    )
+}
+
+pub fn solve_a(input: &str) -> i32 {
+    let connections = parse(input);
+    let possible_end_paths =
+        recursive_find_possible_end_paths(&connections, vec!["start"], true);
+    return possible_end_paths.len() as i32;
+}
+
+pub fn solve_b(input: &str) -> i32 {
+    let connections = parse(input);
+    let possible_end_paths =
+        recursive_find_possible_end_paths(&connections, vec!["start"], false);
+    return possible_end_paths.len() as i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn a_1() {
+        let input = "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+        let result = 10;
+        assert_eq!(solve_a(input), result);
+    }
+    #[test]
+    fn a_2() {
+        let input = "dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+        let result = 19;
+        assert_eq!(solve_a(input), result);
+    }
+    #[test]
+    fn a_3() {
+        let input = "fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+        let result = 226;
+        assert_eq!(solve_a(input), result);
+    }
+    #[test]
+    fn b_1() {
+        let input = "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+        let result = 36;
+        assert_eq!(solve_b(input), result);
+    }
+    #[test]
+    fn b_2() {
+        let input = "dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+        let result = 103;
+        assert_eq!(solve_b(input), result);
+    }
+    #[test]
+    fn b_3() {
+        let input = "fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+        let result = 3509;
+        assert_eq!(solve_b(input), result);
+    }
+}
